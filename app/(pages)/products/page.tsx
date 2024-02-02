337ec34ca67467df0648fa0dcac65c7ce4a533b9
luifean <luisfernandogvv@gmail.com>
2024-02-02 16:00:22 -0300

    feat: Implementando iteração dos produtos mockados a página de produtos

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -1,8 +1,27 @@
-
+import { products } from "@/app/mocks/products"
+import toBRL from "@/app/helpers/to-brl"
 export default function Products(){
 
     return (
-        <h1>Products</h1>
+        <div className="w-full flex-wrap p-10">
+
+            { products.map( product => (
+                <div className="w-[20%] h-[20%] rounded-sm bg-white">
+
+                    <img
+                        width="100%"
+                        height={30} 
+                        alt={product.title} 
+                        src={product.image}/>
+
+                     <h3>{product.title}</h3>
+
+                     <h1>{toBRL(product.value)}</h1>
+
+                </div>
+            ))}
+
+        </div>
     )
 
 }
