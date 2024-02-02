30d988cf93e9f866291ce9863b73dd1d9a84f59b
luifean <luisfernandogvv@gmail.com>
2024-02-02 15:59:04 -0300

    feat: Criando mock de produtos

---
+++ app/mocks/products.ts
@@ -0,0 +1,18 @@
+
+export interface IProduct {
+
+    title: string
+    image: string
+    description: string
+    value: number
+
+}
+
+export const products: IProduct [] = [
+    {
+       title:"Nintendo Switch",
+       image:"https://a-static.mlcdn.com.br/280x210/console-nintendo-switch-oled-com-joy-con-branco-hbgskaaa2/kabum/385192/62b6eb6d59431ab49bcd5380252be4b5.jpeg",
+       description:"Nintendo Switch muito interessante...",
+       value:1.900
+    }
+]
